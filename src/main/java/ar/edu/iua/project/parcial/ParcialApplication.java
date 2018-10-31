8377cf779383dfd95db9c58bd7975fbc4bfcb9a3
GiulianoA <giuliano.alessandro@hotmail.com>
2018-10-31 14:14:27 -0300

    lala

---
+++ src/main/java/ar/edu/iua/project/parcial/ParcialApplication.java
@@ -0,0 +1,12 @@
+package ar.edu.iua.project.parcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialApplication.class, args);
+	}
+}
